647e665c8575b45d0d7510e57678c5f5f40adc55
Anwesh43 <anweshthecool0@gmail.com>
2021-03-04 00:35:43 +0530

    adding BiDotSquareView method

--- bidotthensquareview/src/main/java/com/example/bidotthensquareview/BiDotSquareView.kt
+++ bidotthensquareview/src/main/java/com/example/bidotthensquareview/BiDotSquareView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawBDSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiDotSquare(scale, w, h, paint)
 }
+
+class BiDotSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
