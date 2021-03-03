2f18a06dd225466f93dd71460a71ced8bff1a12f
Anwesh43 <anweshthecool0@gmail.com>
2021-03-04 00:46:25 +0530

    adding Renderer class

--- bidotthensquareview/src/main/java/com/example/bidotthensquareview/BiDotSquareView.kt
+++ bidotthensquareview/src/main/java/com/example/bidotthensquareview/BiDotSquareView.kt
@@ -190,4 +190,27 @@ class BiDotSquareView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiDotSquareView) {
+
+        private val animator : Animator = Animator(view)
+        private val bds : BiDotSquare = BiDotSquare(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bds.draw(canvas, paint)
+            animator.animate {
+                bds.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bds.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
