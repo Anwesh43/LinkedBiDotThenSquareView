c8e8929b6192bc3695ccbf9e3757a13cac937008
Anwesh43 <anweshthecool0@gmail.com>
2021-03-04 00:34:24 +0530

    adding Canvas extensions methods

--- bidotthensquareview/src/main/java/com/example/bidotthensquareview/BiDotSquareView.kt
+++ bidotthensquareview/src/main/java/com/example/bidotthensquareview/BiDotSquareView.kt
@@ -29,3 +29,37 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiDotSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        val rSize : Float = size * sf1
+        save()
+        scale(1f - 2 * j, 1f)
+        save()
+        translate((w / 2 - size / 2) * sf2, 0f)
+        drawRect(RectF(-rSize, -rSize, rSize, rSize), paint)
+        restore()
+        save()
+        drawCircle((w / 2 - size / 2 - size) * sf4, 0f, size * sf3, paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBDSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiDotSquare(scale, w, h, paint)
+}
